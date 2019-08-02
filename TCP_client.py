66d4fee653b4b003f4a75564eab9773fd24beb26
nimesh <nimesh.thakur.cs@gmail.com>
2019-08-02 18:11:31 +0530

    Update TCP_client.py

--- TCP_client.py
+++ TCP_client.py
@@ -1,28 +1,5 @@
-"""
 #client for our connection
 
-from socket import *
-HOST='127.0.0.1'
-PORT=21567
-BUFISZ=1024
-ADDR=(HOST,PORT)
-
-tcp_client_socket=socket(AF_INET,SOCK_STREAM)#creating a client socket
-tcp_client_socket.connect(ADDR)#attemting  a server connection 
-#communication loop
-while True:
-    data=input('>')
-    if not data:
-        break
-    #dialog send and receive 
-    tcp_client_socket.send(data)
-    data=tcp_client_socket.recv(BUFSIZ)
-    if not data:
-        break
-    print(data.decode('utf-8'))
-
-tcp_client_socket.close()
-"""
 from socket import *
 HOST='localhost'
 PORT=21567
